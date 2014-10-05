08769d79f31b798cfe7d61bce84ce162081dbe3d
alankar1985 <alankar.singh1985@gmail.com>
2014-10-05 15:24:40 +0530

    changes 5/10/2014 15:25

--- mysqli.php
+++ mysqli.php
@@ -1,8 +1,33 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+define(DB_HOST, 'localhost');
+define(DB_USER, 'root');
+define(DB_PASS, '');
+define(DB_NAME, 'soccer');
 
+class mysqli {
+
+    private $_connection;
+    private $_host = DB_HOST;
+    private $_username = DB_USER;
+    private $_password = DB_PASS;
+    private $_dbname = DB_NAME;
+    private static $_instance;
+
+    private function __construct() {
+        $this->_connection = new mysqli($this->_host, $this->_username, $this->_password, $this->_dbname);
+// Error handling
+        if (mysqli_connect_error()) {
+            trigger_error("Failed to conencto to MySQL: " . mysql_connect_error(), E_USER_ERROR);
+        }
+    }
+    public function getConnection()
+    {
+        if(!self::$_instance)
+        {
+            self::$_instance=new self();
+        }
+        return self::$_instance;
+    }
+
+}
