82d9707530c97d07d9f9f9656d3c53a5f42c7514
alankar1985 <alankar.singh1985@gmail.com>
2014-10-27 00:08:16 +0530

    sql file to test dtabase class

---
+++ test.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.6
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 26, 2014 at 07:17 PM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `soccer`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `players`
+--
+
+CREATE TABLE IF NOT EXISTS `players` (
+  `p_id` int(11) NOT NULL AUTO_INCREMENT,
+  `t_id` int(11) NOT NULL,
+  `p_fname` varchar(200) NOT NULL,
+  `p_lname` varchar(200) NOT NULL,
+  `p_logo` varchar(200) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`p_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `players`
+--
+
+INSERT INTO `players` (`p_id`, `t_id`, `p_fname`, `p_lname`, `p_logo`, `status`) VALUES
+(1, 1, 'Wayne', 'Rooney', 'wr.jpg', 1),
+(2, 1, 'Marcos', 'Rojo', 'mr.jpg', 1),
+(3, 2, 'Diego', 'Costa', 'dc.jpg', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teams`
+--
+
+CREATE TABLE IF NOT EXISTS `teams` (
+  `t_id` int(11) NOT NULL AUTO_INCREMENT,
+  `t_name` varchar(200) NOT NULL,
+  `t_logo` varchar(200) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`t_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `teams`
+--
+
+INSERT INTO `teams` (`t_id`, `t_name`, `t_logo`, `status`) VALUES
+(1, 'Manchester United', 'manu.png', 1),
+(2, 'Chelsea', 'chelsea.jpg', 1);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
